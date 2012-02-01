7357df20843a44a40a7690dac8c2c2894910e1ea
Ross Boucher <rboucher@gmail.com>
2012-02-01 11:28:12 -0800

    Add the ability to create a test charge.

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ var emitter = new EventEmitter;
 
 var express = require('express');
 var app = express.createServer();
+var stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 app.use(express.static(__dirname));
 app.use(express.bodyParser())
+
+app.post('/test-payment', function(request, response){
+	stripe.charges.create({
+		currency: 'usd',
+		amount: Math.floor(request.body.amount*100) || 100,
+		description: request.body.description || "",
+		card: {
+			number: '4242424242424242',
+			exp_month: '12',
+			exp_year: '2018'
+		}
+	}, function(err, charge){
+		response.send(JSON.stringify(charge));
+	})
+});
 
 app.post('/stripe-webhook', function(request, response){
 	console.log(request.body)
